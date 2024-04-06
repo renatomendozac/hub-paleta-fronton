df5f1a2915a2a99a74e78b2df194662589e6a304
Renato Mendoza <renatomendozac@gmail.com>
2024-04-05 22:32:01 -0500

    chore: add content to main page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 const content = [
   {
     title: 'Torneo Metropolitano Villa 2023, semis, sub18, Paleta Frontón, Julián Portaro vs Santiago López',
@@ -33,23 +35,15 @@ const content = [
 
 export default function Home () {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hub - Videos de paleta fronton
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+      <h1>Hub - Videos de paleta fronton</h1>
 
-      <ul style={{ display: 'grid', gridTemplateColumns: 'repeat(3, minmax(0, 1fr))', gap: '2rem' }}>
+      <ul className='grid grid-cols-3 gap-4'>
         {
           content.map(({ title, id }) => (
-            <li key="title">
+            <li key={id}>
               <h1>{title}</h1>
-              <img src={`https://img.youtube.com/vi/${id}/0.jpg`}></img>
-              {/* <iframe
-                src={link}
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerpolicy="strict-origin-when-cross-origin"
-                allowfullscreen
-              /> */}
+              <Image alt={title} src={`https://img.youtube.com/vi/${id}/0.jpg`} width={500} height={500} />
             </li>
           ))
         }
