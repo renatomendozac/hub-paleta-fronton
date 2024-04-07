8a1bfa681c403af309345da3c5ef125a467b89cc
Renato Mendoza <renatomendozac@gmail.com>
2024-04-07 17:19:07 -0500

    feat: add Tag and Header components

---
+++ components/Tag.js
@@ -0,0 +1,9 @@
+const Tag = ({ label }) => {
+  return (
+    <span className={'text-xs py-1 px-2 m-1 rounded bg-main text-main font-bold'}>
+      {label}
+    </span>
+  )
+}
+
+export default Tag
