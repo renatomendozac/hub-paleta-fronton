3d87882551bbe3f1c98a4301eb413c2f4ec27172
Renato Mendoza <renatomendozac@gmail.com>
2024-04-07 17:19:28 -0500

    add match page

---
+++ app/match/[id]/page.js
@@ -0,0 +1,38 @@
+import Tag from '@/components/Tag'
+import { supabase } from '@/lib/supabase'
+
+const Match = async ({ params }) => {
+  const { data: matches } = await supabase
+    .from('match')
+    .select('*')
+    .eq('id', params.id)
+
+  if (!(matches && matches.length)) {
+    return <main>No se ha encontrado el partido.</main>
+  }
+
+  const [{ title, link, tags }] = matches
+
+  return (
+    <>
+      <h1 className='mb-6'>{title}</h1>
+
+      <div className='relative w-full pb-[56.25%] overflow-hidden'>
+        <iframe
+          src={`https://www.youtube.com/embed/${link}`}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className='absolute top-0 left-0 w-full h-full'
+        />
+      </div>
+
+      <div className='mt-6'>
+        {tags.map(label => <Tag key={label} label={label} />)}
+      </div>
+    </>
+  )
+}
+
+export default Match
