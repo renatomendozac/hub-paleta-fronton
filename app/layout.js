db17b41671118652d176895d0df9fe22ba70da3e
Renato Mendoza <renatomendozac@gmail.com>
2024-04-07 15:42:15 -0500

    feat: add logo and favicon

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Hub paleta fronton',
-  description: 'Hub de videos de paleta fronton peruano.'
+  title: 'Partidos de paleta fronton',
+  description: 'Partidos de paleta fronton peruano.'
 }
 
 export default function RootLayout ({ children }) {
