b9b68ccae0bf5f438a2b7c409b08711d9ca21d49
Renato Mendoza <renatomendozac@gmail.com>
2024-04-07 17:19:28 -0500

    add match page

--- app/page.js
+++ app/page.js
@@ -8,28 +8,12 @@ export default async function Home () {
     .select('*')
 
   return (
-    <main className="min-h-screen p-12">
-      <header className='flex-col flex justify-between mb-8 gap-4 sm:items-center sm:flex-row'>
-        <div className='flex items-center gap-4'>
-          <Link href="/">
-            <Image
-              src="/logo.svg"
-              alt="Logo"
-              width={64}
-              height={64}
-              priority
-            />
-          </Link>
-          <h1 className='text-2xl'>Partidos de paleta frontón</h1>
-        </div>
-        <Link className='text-right' href="/contributing">Contribuir</Link>
-      </header>
-
-      <ul className='grid grid-cols-1 gap-4 list-none sm:grid-cols-2 md:grid-cols-3 items-end'>
-        {
-          matches.map(({ title, link }) => (
-            <li key={link}>
-              <h1 className='mb-2'>{title}</h1>
+    <ul className='grid grid-cols-1 gap-5 list-none sm:grid-cols-2 md:grid-cols-3 items-end'>
+      {
+        matches.map(({ id, title, link }) => (
+          <li key={id}>
+            <h1 className='mb-2'>{title}</h1>
+            <Link href={`/match/${id}`}>
               <Image
                 priority
                 alt={title}
@@ -37,10 +21,10 @@ export default async function Home () {
                 width={500}
                 height={500}
               />
-            </li>
-          ))
-        }
-      </ul>
-    </main>
+            </Link>
+          </li>
+        ))
+      }
+    </ul>
   )
 }
