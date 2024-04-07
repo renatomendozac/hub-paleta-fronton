ddfd33b2baab353be9fabd9fcfbdb49775495759
Renato Mendoza <renatomendozac@gmail.com>
2024-04-07 10:05:27 -0500

    feat: add supabase

--- app/page.js
+++ app/page.js
@@ -1,49 +1,27 @@
+import { supabase } from '@/lib/supabase'
 import Image from 'next/image'
 
-const content = [
-  {
-    title: 'Torneo Metropolitano Villa 2023, semis, sub18, Paleta Frontón, Julián Portaro vs Santiago López',
-    id: 'NlDpCXjxITU',
-    platform: 'youtube'
-  },
-  {
-    title: 'Torneo Metropolitano Villa 2023, semis, sub18, Paleta Frontón, Julián Portaro vs Santiago López',
-    id: 'NlDpCXjxITU',
-    platform: 'youtube'
-  },
-  {
-    title: 'Torneo Metropolitano Villa 2023, semis, sub18, Paleta Frontón, Julián Portaro vs Santiago López',
-    id: 'NlDpCXjxITU',
-    platform: 'youtube'
-  },
-  {
-    title: 'Torneo Metropolitano Villa 2023, semis, sub18, Paleta Frontón, Julián Portaro vs Santiago López',
-    id: 'NlDpCXjxITU',
-    platform: 'youtube'
-  },
-  {
-    title: 'Torneo Metropolitano Villa 2023, semis, sub18, Paleta Frontón, Julián Portaro vs Santiago López',
-    id: 'NlDpCXjxITU',
-    platform: 'youtube'
-  },
-  {
-    title: 'Torneo Metropolitano Villa 2023, semis, sub18, Paleta Frontón, Julián Portaro vs Santiago López',
-    id: 'NlDpCXjxITU',
-    platform: 'youtube'
-  }
-]
+export default async function Home () {
+  const { data: matches } = await supabase
+    .from('match')
+    .select('*')
 
-export default function Home () {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
-      <h1>Hub - Videos de paleta fronton</h1>
+      <h1 className='text-center'>Hub - Videos de paleta fronton</h1>
 
-      <ul className='grid grid-cols-1 gap-4 box-border list-none my-8 sm:grid-cols-2  md:grid-cols-3'>
+      <ul className='grid grid-cols-1 gap-4 box-border list-none my-8 sm:grid-cols-2 md:grid-cols-3 items-end'>
         {
-          content.map(({ title, id }) => (
-            <li key={id}>
+          matches.map(({ title, link }) => (
+            <li key={link}>
               <h1>{title}</h1>
-              <Image alt={title} src={`https://img.youtube.com/vi/${id}/0.jpg`} width={500} height={500} />
+              <Image
+                priority
+                alt={title}
+                src={`https://img.youtube.com/vi/${link}/0.jpg`}
+                width={500}
+                height={500}
+              />
             </li>
           ))
         }
