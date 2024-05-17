ff3f334423e8e03af197fe8190b42d35551d0e28
Renato Mendoza <renatomendozac@gmail.com>
2024-05-17 13:30:55 -0500

    fix: update classname from Alert component

--- components/Alert.js
+++ components/Alert.js
@@ -7,7 +7,7 @@ const Alert = ({ type = 'error', children }) => {
     : [CircleXIcon, 'bg-red-700']
 
   return (
-    <div className='fixed top-[-60px] left-0 w-full flex justify-center z-50 anirenat'>
+    <div className='fixed top-[-60px] left-0 w-full flex justify-center z-50 alert-translate'>
       <div className={`w-full max-w-xs text-sm ${bgColor} text-stone-300 p-2 rounded-md`}>
         <Icon width={22} height={22} className="inline-block" />
         <span className='pl-1'>{children}</span>
