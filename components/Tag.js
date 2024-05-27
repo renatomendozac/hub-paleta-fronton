04b1ef7276d43d7c68937efbaaebbba2564b2dcf
Renato Mendoza <renatomendozac@gmail.com>
2024-05-27 17:08:52 -0500

    feat: create ListMatches component

--- components/Tag.js
+++ components/Tag.js
@@ -1,7 +1,7 @@
 const Tag = ({ label, onClick }) => {
   return (
     <span
-      className={'text-xs py-1 px-2 m-1 rounded bg-main text-main font-bold cursor-pointer'}
+      className="text-xs py-1 px-2 m-1 rounded bg-main text-main font-bold cursor-pointer"
       onClick={onClick}
     >
       {label}
