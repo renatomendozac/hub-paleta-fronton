cee09bf964d392a91e1fccb3fa10ad2056fa876e
Renato Mendoza <renatomendozac@gmail.com>
2024-06-10 13:20:07 -0500

    feat: add Modal and Overlay component

---
+++ components/Modal.js
@@ -0,0 +1,82 @@
+import Overlay from './Overlay'
+
+const Modal = ({
+  title,
+  children,
+  isVisible,
+  setIsVisible,
+  primaryButtonText,
+  primaryButtonAction,
+  secondaryButtonText,
+  secondaryButtonAction
+}) => {
+  return (
+    <>
+      { isVisible && <Overlay /> }
+
+      <div
+        id="small-modal"
+        tabIndex="-1"
+        className={`fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full flex justify-center items-center ${!isVisible && 'hidden'}`}
+      >
+        <div className="relative w-full max-w-md max-h-full">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+              <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+                {title}
+              </h3>
+              {
+                setIsVisible && (
+                  <button
+                    type="button"
+                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-hide="small-modal"
+                    onClick={() => setIsVisible(false)}
+                  >
+                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span className="sr-only">Close modal</span>
+                  </button>
+                )
+              }
+            </div>
+            <div className="p-4 md:p-5 space-y-4">
+              <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                {children}
+              </p>
+            </div>
+            <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+              {
+                primaryButtonText && primaryButtonAction && (
+                  <button
+                    data-modal-hide="small-modal"
+                    type="button"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    onClick={primaryButtonAction}
+                  >
+                    {primaryButtonText}
+                  </button>
+                )
+              }
+              {
+                secondaryButtonText && secondaryButtonAction && (
+                  <button
+                    data-modal-hide="small-modal"
+                    type="button"
+                    className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                    onClick={secondaryButtonAction}
+                  >
+                    {secondaryButtonText}
+                  </button>
+                )
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Modal
