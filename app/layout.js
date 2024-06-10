a924514572547dcf80ec0af3491fd02e6fc0a086
Renato Mendoza <renatomendozac@gmail.com>
2024-06-10 17:35:37 -0500

    feat: add revalidation equal to zero

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,8 @@ export const metadata = {
   description: 'Partidos de paleta fronton peruano.'
 }
 
+export const revalidate = 0
+
 export default function RootLayout ({ children }) {
   return (
     <html lang='es'>
