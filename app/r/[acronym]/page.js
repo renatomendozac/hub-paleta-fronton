3eb6974e595a3c68b9ee5e28d2c10b02522ad7b8
Renato Mendoza <renatomendozac@gmail.com>
2024-10-25 18:45:04 -0500

    feat: add ShortLinkRedirect page

---
+++ app/r/[acronym]/page.js
@@ -0,0 +1,10 @@
+import { getCompetitionByAcronym } from '@/services/get-competition-by-acronym'
+import { redirect } from 'next/navigation'
+
+export const revalidate = 0
+
+export default async function ShortLinkRedirect ({ params }) {
+  const { acronym } = params
+  const competitionName = await getCompetitionByAcronym(acronym)
+  redirect(`/competition/${competitionName}`)
+}
