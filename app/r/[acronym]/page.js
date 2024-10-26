e48abfe2f9ff601c4d69e54e3763ff22b1f98b4d
jafetmendozac <abrahamjoelmc@gmail.com>
2024-10-25 20:52:14 -0500

    correction to redirect

--- app/r/[acronym]/page.js
+++ app/r/[acronym]/page.js
@@ -6,5 +6,6 @@ export const revalidate = 0
 export default async function ShortLinkRedirect ({ params }) {
   const { acronym } = params
   const competitionName = await getCompetitionByAcronym(acronym)
+  encodeURIComponent(competitionName)
   redirect(`/competition/${competitionName}`)
 }
