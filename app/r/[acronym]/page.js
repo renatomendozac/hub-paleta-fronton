636d75835f46a749bb582324a2598d29b12b58df
Renato Mendoza <renatomendozac@gmail.com>
2024-10-25 22:53:42 -0500

    Revert "correction"
    This reverts commit b0c1cabfeb2668c1583af80e2ea8d6cb48eb2291.

--- app/r/[acronym]/page.js
+++ app/r/[acronym]/page.js
@@ -6,5 +6,6 @@ export const revalidate = 0
 export default async function ShortLinkRedirect ({ params }) {
   const { acronym } = params
   const competitionName = await getCompetitionByAcronym(acronym)
-  redirect(`/competition/${encodeURIComponent(competitionName)}`)
+  encodeURIComponent(competitionName)
+  redirect(`/competition/${competitionName}`)
 }
