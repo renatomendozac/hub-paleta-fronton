de4919b43f26552603e78bbdca6fde722c26de04
Renato Mendoza <renatomendozac@gmail.com>
2024-12-16 16:26:17 -0500

    feat: sort by game date and publication

--- services/get-list-matches.js
+++ services/get-list-matches.js
@@ -7,10 +7,10 @@ export const getListMatches = async ({ type, label }, columns = '*') => {
   switch (type) {
     case FilterBy.Tag: {
       if (label === 'TCN') {
-        const { data } = await supabase.from('match').select(`${columns}, competition!inner(is_tcn)`).eq('competition.is_tcn', true).order('date', { ascending: false })
+        const { data } = await supabase.from('match').select(`${columns}, competition!inner(is_tcn)`).eq('competition.is_tcn', true).order('date', { ascending: false }).order('created', { ascending: false })
         results = data
       } else {
-        const { data } = await supabase.from('match').select(columns).contains('tags', [label]).order('date', { ascending: false })
+        const { data } = await supabase.from('match').select(columns).contains('tags', [label]).order('date', { ascending: false }).order('created', { ascending: false })
         results = data
       }
       break
@@ -19,7 +19,7 @@ export const getListMatches = async ({ type, label }, columns = '*') => {
     case FilterBy.Competition: {
       const { data: competition } = await supabase.from('competition').select('id').eq('name', label).single()
       if (competition) {
-        const { data } = await supabase.from('match').select(columns).eq('competition', competition.id).order('date', { ascending: false })
+        const { data } = await supabase.from('match').select(columns).eq('competition', competition.id).order('date', { ascending: false }).order('created', { ascending: false })
         results = data
       }
       break
@@ -28,7 +28,7 @@ export const getListMatches = async ({ type, label }, columns = '*') => {
     case FilterBy.Category: {
       const { data: category } = await supabase.from('category').select('id').eq('acronym', label).single()
       if (category) {
-        const { data } = await supabase.from('match').select(columns).eq('category', category.id).order('date', { ascending: false })
+        const { data } = await supabase.from('match').select(columns).eq('category', category.id).order('date', { ascending: false }).order('created', { ascending: false })
         results = data
       }
       break
@@ -37,14 +37,14 @@ export const getListMatches = async ({ type, label }, columns = '*') => {
     case FilterBy.Player: {
       const { data: player } = await supabase.from('player').select('id').eq('full_name', label).single()
       if (player) {
-        const { data } = await supabase.from('match').select(columns).contains('players', [player.id]).order('date', { ascending: false })
+        const { data } = await supabase.from('match').select(columns).contains('players', [player.id]).order('date', { ascending: false }).order('created', { ascending: false })
         results = data
       }
       break
     }
 
     case FilterBy.Visible: {
-      const { data } = await supabase.from('match').select(columns).eq('is_visible', true).order('date', { ascending: false })
+      const { data } = await supabase.from('match').select(columns).eq('is_visible', true).order('date', { ascending: false }).order('created', { ascending: false })
       results = data
       break
     }
